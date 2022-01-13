2acc37cc89d75ef759c89ac7a99cca0ebab95b00
H-Builder <high-builder@hotmail.com>
2022-01-13 14:06:07 -0500

    feat: prettier implementation

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,13 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
 
-function App() {
+import "./App.css"
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <canvas id="myCanvas" width="600" height="400">
+      Your browser does not support the HTML5 canvas tag.
+    </canvas>
+  )
 }
 
-export default App;
+export default App
